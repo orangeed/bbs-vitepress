6fec288426b16b170622b11aed55da30699886e5
橘子orange <1208917130@qq.com>
2026-05-20 18:08:16 +0800

    feat(docs): 新增iframe嵌入组件和旧网站资源页面
    新增IframeEmbed.vue组件用于嵌入外部内容，
    添加旧网站资源汇总页面，包含腾讯文档链接

---
+++ docs/components/IframeEmbed.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="iframe-wrapper">
+    <iframe :src="src" :height="height" :width="width" frameborder="0"></iframe>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const props = defineProps({
+  src: String,
+  height: { type: [String, Number], default: 500 },
+  width: { type: [String, Number], default: 1000 },
+})
+</script>
