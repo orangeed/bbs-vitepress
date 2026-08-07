96d031d43bbc8adbf6e2b58c4e3720f7b1d26638
橘子orange <1208917130@qq.com>
2026-08-07 09:51:40 +0800

    feat: 新增掘金小册

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,19 @@ export default defineConfig({
   title: "橘子的分享",
   description: "一个基于 VitePress 与 animal-island-vue 打造的动森风格博客",
   lang: "zh-CN",
-  head: [["meta", { name: "referrer", content: "no-referrer" }]],
+  head: [
+    ["meta", { name: "referrer", content: "no-referrer" }],
+    // 51la 数据统计
+    [
+      "script",
+      {
+        charset: "UTF-8",
+        id: "LA_COLLECT",
+        src: "//sdk.51.la/js-sdk-pro.min.js",
+      },
+    ],
+    ["script", {}, `LA.init({id: "3Pza9oQ74JEgxtxN", ck: "3Pza9oQ74JEgxtxN"})`],
+  ],
   vite: {
     ssr: {
       // 该组件库依赖浏览器 API，构建时需避免 SSR 转换出错
